bb513ab68abcbfd34aa21053cdb20d7916ec0cfc
Vae1ocks <antondobranickij@gmail.com>
2024-09-01 13:06:48 +0300

    Поправил url к документации

--- user_service/user_service/urls.py
+++ user_service/user_service/urls.py
@@ -6,8 +6,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path('users/api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('users/api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('users/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('users/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
     path('users/', include('users.urls', namespace='users')),
     path('users/payout/', include('payout.urls', namespace='payout')),
